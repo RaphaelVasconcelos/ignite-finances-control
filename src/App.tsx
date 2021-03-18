4cd1e3d9bf90005a39b7dcca41411f4d6b54ea06
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-03-18 05:58:01 -0300

    Creating app layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import {Dashboard } from "./components/Dashboard"
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
+    <>
+      <Header/>
+      <Dashboard/>
+      <GlobalStyle/>
+    </>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --background: #f0f2f5;
+        --red: #E52E4D;
+        --green: #33CC95;
+        --blue: #5429CC;
+        
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+
+        --shape: #FFFFFF;
+    }
+
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box
+    }
+
+    // font-size padrao: 16px (Desktop)
+
+    html{
+        @media (max-width: 1080px) {
+            font-size: 93.75%; //16*0,9375 15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; //16*0,875 14px
+        }
+    }
+
+    body{
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family: 'Popins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, strong{
+        font-weight: 600;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disable]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
