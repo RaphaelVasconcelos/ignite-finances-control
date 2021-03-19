530f8dd8d4ff924c24d969e627ba28deee96faeb
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-03-19 05:34:12 -0300

    Adding styles to type transactions  buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,11 @@
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
-import { Container } from "../NewTransactionModal/styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import {
+  Container,
+  TransactionTypeContainer,
+} from "../NewTransactionModal/styles";
 
 interface NewTransaciontModalProps {
   isOpen: boolean;
@@ -29,6 +34,18 @@ export function NewTransactionModal({
         <h2>Cadastrar Transação</h2>
         <input placeholder="Titulo" />
         <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
