7ad3add2be2ee962865e1d6a4e42b160aee46a1b
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-03-19 06:22:14 -0300

    Adding state to form input and config to save on mirage

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import {
   Container,
   TransactionTypeContainer,
@@ -20,6 +21,18 @@ export function NewTransactionModal({
 }: NewTransaciontModalProps) {
   const [type, setType] = useState("deposit");
 
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
+
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault();
+    const data = {title, value, category, type};
+
+    api.post('/transaction', data);
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -34,10 +47,10 @@ export function NewTransactionModal({
       >
         <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
-        <input placeholder="Titulo" />
-        <input placeholder="Valor" type="number" />
+        <input placeholder="Titulo" value={title} onChange={event => setTitle(event.target.value)} />
+        <input placeholder="Valor" type="number" value={value} onChange={event => setValue(Number(event.target.value))} />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -64,7 +77,7 @@ export function NewTransactionModal({
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input placeholder="Categoria" value={category} onChange={event => setCategory(event.target.value)}/>
 
         <button type="submit">Cadastrar</button>
       </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 createServer({
-  routes() {
-    this.namespace = '/api';
+  models: {
+    transaction: Model,
+  },
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date() 
-        }
-      ]
-    })
-  }
-})
+  routes() {
+    this.namespace = "/api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transactions");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
