b307bee370619332547f524ed410cdbd5d548e4c
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-03-22 19:14:50 -0300

    Creating context to transactions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { GlobalStyle } from "./styles/global";
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { useState } from "react";
+import { TransactionsProvider} from './TransactionsContext';
+
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
@@ -21,11 +23,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
