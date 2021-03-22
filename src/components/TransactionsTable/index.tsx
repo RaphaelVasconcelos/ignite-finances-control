ed1252ca2a3c53b798b0bb0ef5723bbb179105b9
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-03-22 06:15:13 -0300

    Creating Seeds and setup list of transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,40 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "./styles";
-import { api } from "../../services/api"
+import { api } from "../../services/api";
 
-export function TransactionsTable (){
-    useEffect(() => {
-        api.get('http://localhost:3000/api/transactions')
-            .then(response => console.log(response.data))
-    }, []);
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Titulo</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de site</td>
-                        <td className="deposit">R$ 12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$ 1.000</td>
-                        <td>Casa</td>
-                        <td>17/02/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    );
+  useEffect(() => {
+    api.get('transactions')
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map((transaction) => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Container>
+  );
 }
